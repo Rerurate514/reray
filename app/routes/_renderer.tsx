14da01a04221463491f389f0092cd6c3038eb355
Rerurate514 <rerurateyuto@gmail.com>
2026-09-18 02:05:20 +0900

    desc追加

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -3,15 +3,16 @@ import { Link, Script } from 'honox/server'
 
 export default jsxRenderer(({ children }) => {
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Reray は、いつでも作れる記事リレーサービスです。" />
         <link rel="icon" href="/favicon.ico" />
         <Link href="/app/style.css" rel="stylesheet" />
         <Script src="/app/client.ts" async />
       </head>
-      <body>{children}</body>
+      <body class="bg-[#f7f4ef] text-[#1f2933]">{children}</body>
     </html>
   )
 })
